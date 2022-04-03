8c4cdd66d4f05e9d4eefda46a1659adade9445fa
UzairQamarxyz <uzairq@xgrid.co>
2022-04-03 16:17:40 +0500

    Updating with new patches

--- config.def.h
+++ config.def.h
@@ -3,11 +3,11 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Barlow:size=12"
+	"Iosevka Custom:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -18,8 +18,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 5;
+static unsigned int columns    = 1;
 static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
 
 /*
@@ -33,4 +34,4 @@ static const char worddelimiters[] = " ";
  */
 static int use_prefix = 1;
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 2;
